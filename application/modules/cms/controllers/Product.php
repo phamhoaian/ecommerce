86cc962040715b84f5c8525a4f7beefeedc1e2f1
phamhoaian <phamhoaian005@gmail.com>
2016-08-04 23:16:56 +0700

    add product form

--- application/modules/cms/controllers/Product.php
+++ application/modules/cms/controllers/Product.php
@@ -29,7 +29,7 @@ class Product extends MY_Controller {
 		$this->data["user_name"] = $this->get_username();
 
 		// limit per page
-		$this->limit = 1;
+		$this->limit = 10;
 	}
 
 	public function index() {
@@ -129,5 +129,257 @@ class Product extends MY_Controller {
 
 		// load view
 		$this->load_view('product/search', $this->data);
+	}
+
+	/**
+	 * Add new or edit a product
+	 * @param id integer (NULL: add; !=NULL: edit)
+	 * @return void
+	 */
+	public function form()
+	{
+		// menu active
+		$this->data['menu_active'] = "product";
+		$this->data['submenu_active'] = "product";
+		
+		// get product id 
+		$this->data["id"] = $this->security_clean($this->uri->segment(4, 0));
+
+		// page title
+		if ($this->data["id"])
+		{
+			$this->data["page_title"] = "Chỉnh sửa thông tin sản phẩm";
+		}
+		else
+		{
+			$this->data["page_title"] = "Thêm sản phẩm mới";
+		}
+
+		// prepare product information
+		if($this->data["id"])
+		{
+			$this->data["product"] = $this->common_model->get_product_by_id($this->data["id"]);
+			if(!$this->data["product"])
+			{
+				$this->session->set_flashdata("error", "Sản phẩm không tồn tại!");
+                redirect("cms/product/search");
+			}
+		}
+		else
+		{
+			$this->data["product"] = array(
+				'name' => '',
+				'cat_id' => '',
+				'maker_id' => '',
+				'price' => '',
+				'content' => '',
+				'discount' => '',
+				'image_link' => '',
+				'image_list' => '',
+				'site_title' => '',
+				'warranty' => '',
+				'gifts' => '',
+				'video' => '',
+				'feature' => '',
+				'meta_key' => '',
+				'meta_desc' => '',
+				'created' => ''
+			);
+		}
+
+		// get list categories
+		$this->data['list_categories'] = $this->common_model->get_all_categories(array('parent_id' => 0), 'id ASC');
+		if($this->data['list_categories'])
+		{
+			foreach ($this->data['list_categories'] as &$row) {
+				$row['child'] = $this->common_model->get_all_categories(array('parent_id' => $row['id']), 'id ASC');
+			}
+		}
+
+		// form validation
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('name', 'Tên', 'trim|required|xss_clean|callback_chk_product_name_duplicate');
+		$this->form_validation->set_rules('cat_id', 'Thể loại', 'trim|required|xss_clean');
+		$this->form_validation->set_rules('maker_id', 'Nhà sản xuất', 'trim|xss_clean');
+		$this->form_validation->set_rules('price', 'Giá', 'trim|required|xss_clean');
+		$this->form_validation->set_rules('content', 'Mô tả', 'trim|xss_clean');
+		$this->form_validation->set_rules('discount', 'Giảm giá', 'trim|xss_clean');
+		$this->form_validation->set_rules('image_link', 'Ảnh đại diện', 'trim|xss_clean');
+		$this->form_validation->set_rules('image_list', 'Ảnh kèm theo', 'trim|xss_clean');
+		$this->form_validation->set_rules('site_title', 'Tiêu đề trang', 'trim|xss_clean');
+		$this->form_validation->set_rules('warranty', 'Bảo hành', 'trim|xss_clean');
+		$this->form_validation->set_rules('gifts', 'Quà tặng', 'trim|xss_clean');
+		$this->form_validation->set_rules('video', 'Video', 'trim|xss_clean');
+		$this->form_validation->set_rules('feature', 'Nổi bật', 'trim|xss_clean');
+		$this->form_validation->set_rules('meta_key', 'Meta key', 'trim|xss_clean');
+		$this->form_validation->set_rules('meta_desc', 'Meta description', 'trim|xss_clean');
+		$this->form_validation->set_rules('created', 'Ngày tạo', 'trim|xss_clean');
+
+		if($this->form_validation->run())
+		{
+			// prepare user data
+			$upd_data = array(
+				'name' => $this->security_clean(set_value('name')),
+				'cat_id' => intval($this->security_clean(set_value('cat_id'))),
+				'maker_id' => intval($this->security_clean(set_value('maker_id'))),
+				'price' => $this->security_clean(set_value('price')),
+				'content' => htmlspecialchars($this->security_clean(set_value('content'))),
+				'discount' => $this->security_clean(set_value('discount')),
+				'image_link' => $this->security_clean(set_value('image_link')),
+				'image_list' => $this->security_clean(set_value('image_list')),
+				'site_title' => $this->security_clean(set_value('site_title')),
+				'warranty' => $this->security_clean(set_value('warranty')),
+				'gifts' => $this->security_clean(set_value('gifts')),
+				'video' => $this->security_clean(set_value('video')),
+				'feature' => $this->security_clean(set_value('feature')),
+				'meta_key' => htmlspecialchars($this->security_clean(set_value('meta_key'))),
+				'meta_desc' => htmlspecialchars($this->security_clean(set_value('meta_desc')))
+			);
+
+			// insert data into table product
+			$message = "";
+			$this->common_model->set_table('product');
+			if($this->data["id"]) // in case update
+			{
+				if($this->common_model->update($upd_data, array('id' => $this->data["id"])))
+				{
+					$message = "Cập nhật thông tin sản phẩm thành công!";
+				}
+				else 
+				{
+					$message = "Cập nhật thất bại!";
+				}
+			}
+			else // in case insert
+			{
+				$upd_data['created'] = date('Y-m-d H:i:s', time());
+				if($this->common_model->insert($upd_data))
+				{
+					$message = "Thêm mới sản phẩm thành công!";
+				}
+				else
+				{
+					$message = "Thêm mới sản phẩm thất bại!";
+				}
+			}
+
+			$this->session->set_flashdata("message", $message);
+            redirect("cms/product/search");
+		} 
+		else 
+		{
+			// load view
+			$this->load_view('product/form', $this->data);
+		}
+	}
+
+	/**
+	 *
+	 * Delete a product by product_id
+	 * @param  integer product_id
+	 * @return boolean
+	 *
+	 */
+	public function del()
+	{
+		$product_id = $this->security_clean($this->uri->segment(4, 0));
+		if(is_numeric($product_id) && $product_id)
+		{
+			$product = $this->common_model->get_product_by_id($product_id);
+			if($product)
+			{
+				// delete product
+				$this->common_model->set_table('product');
+				$del_flag = $this->common_model->delete(array('id' => $product_id));
+				if($del_flag)
+				{
+					$message = "Xóa sản phẩm thành công!";
+					$this->session->set_flashdata("message", $message);
+            		redirect("cms/product");
+					return TRUE;
+				}
+				else // in case no data
+				{
+					define('RETURN_URL', site_url("cms/product"));
+					$this->message("Không có dữ liệu!");
+					return FALSE;
+				}
+			}
+			else // in case product doesn't exists
+			{
+				define('RETURN_URL', site_url("cms/product"));
+				$this->message("Sản phẩm không tồn tại!");
+				return FALSE;
+			}
+		}
+		else // in case product_id isn't integer or empty
+		{
+			define('RETURN_URL', site_url("cms/product"));
+			$this->message("Truy cập bị từ chối!");
+			return FALSE;
+		}
+	}
+
+	/**
+	 *
+	 * Delete multi categories by list product_id
+	 * @param  array product_ids
+	 * @return boolean
+	 *
+	 */
+	public function del_all()
+	{
+		// status flag
+		$status = FALSE;
+
+		// get list product_id
+		$product_ids = $this->security_clean($this->input->post('ids'));
+		
+		if ($product_ids)
+		{
+			$product_ids = implode(",", $product_ids);
+
+			$this->common_model->set_table('product');
+			$del_flag = $this->common_model->delete(array("id IN({$product_ids})" => NULL));
+			if($del_flag)
+			{
+				$status = TRUE;
+			}
+		}
+		return $status;
+	}
+
+	/**
+	 *
+	 * Check product name duplicate
+	 * @param string @name
+	 * @return boolean
+	 *
+	 */
+	public function chk_product_name_duplicate($name)
+	{
+		$this->common_model->set_table('product');
+		if($this->data["id"]) // in case edit
+		{
+			$where = array(
+				'name' => $name,
+				'id !=' => $this->data["id"]
+			);
+		}
+		else // in case insert
+		{
+			$where = array(
+				'name' => $name
+			);
+		}
+
+		if($this->common_model->get_row($where))
+		{
+			$this->form_validation->set_message('chk_product_name_duplicate', 'Tên sản phẩm này đã có! Vui lòng nhập tên khác.');
+			return FALSE;
+		}
+
+		return TRUE;
 	}
 }
