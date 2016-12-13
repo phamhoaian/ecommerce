f15e1af4fc6a88451bccc03649e296d4069054a4
anph <anph@bit-vietnam.com>
2016-12-13 16:36:10 +0700

    fix sideci

--- application/modules/home/views/side_ci.php
+++ application/modules/home/views/side_ci.php
@@ -1,5 +1,5 @@
-<h3 class="box-title"><i class="fa fa-building"></i>
 <?php echo $exhibition['name']; ?>
+<h3 class="box-title"><i class="fa fa-building"></i>
 に加盟している会社情報編集</h3>
 <!-- End of file side_ci.php -->
 <!-- Location: ./modules/home/views/side_ci.php -->
